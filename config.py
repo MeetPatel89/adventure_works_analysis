97e65add274af4d3205624658b8a92ebfbf51971
Meet Patel <meetaiims@yahoo.com>
2023-05-20 15:32:08 -0400

    add transform_products.ipynb notebook to transform aw_products from raw to processed dataset for powerbi presentation

--- config.py
+++ config.py
@@ -4,7 +4,11 @@ from dotenv import load_dotenv
 # load the .env file
 load_dotenv()
 
-ROOT_DIR = os.path.abspath(os.curdir)
+# get root directory based on location of config.py
+def func_get_root_dir():
+    return os.path.abspath(os.curdir)
+ROOT_DIR = func_get_root_dir()
+
 SQL_USER=os.getenv("SQL_USER")
 SQL_PWD=os.getenv("SQL_PWD")
 SQL_SERVER=os.getenv("SQL_SERVER")
